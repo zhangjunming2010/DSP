5cce2eecbd8113493c914457e353a36bdaa68735
zhangjunming2010 <zhangjunming2010@163.com>
2017-03-28 23:21:52 +0800

    1.增加验证码生成

--- src/main/java/com/tinymore/dsp/resource/LoginResource.java
+++ src/main/java/com/tinymore/dsp/resource/LoginResource.java
@@ -1,24 +1,48 @@
 package com.tinymore.dsp.resource;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tinymore.dsp.utils.VerifyCodeUtil;
 
 @Controller  
-@RequestMapping("/login")
+@RequestMapping("/")
 @CrossOrigin(origins="*")
 public class LoginResource {
 	
 	private static final Logger log = (Logger) LogManager.getLogger(LoginResource.class);
 	
-	@RequestMapping(value = "/getCode",method = RequestMethod.POST,produces="application/json; charset=utf-8")
-	@ResponseBody
-	public void getCode() {
-		
+	@RequestMapping(value = "/getCode",method = RequestMethod.GET,produces="application/json; charset=utf-8")
+	public void getCode(HttpServletRequest request, HttpServletResponse response) {
+		response.setHeader("Pragma", "No-cache"); 
+        response.setHeader("Cache-Control", "no-cache"); 
+        response.setDateHeader("Expires", 0); 
+        response.setContentType("image/jpeg"); 
+           
+        //生成随机字串 
+        String verifyCode = VerifyCodeUtil.generateVerifyCode(4); 
+        //存入会话session 
+        HttpSession session = request.getSession(true); 
+        //删除以前的
+        session.removeAttribute("verCode");
+        session.setAttribute("verCode", verifyCode.toLowerCase()); 
+        //生成图片 
+        int w = 100, h = 42; 
+        try {
+			VerifyCodeUtil.outputImage(w, h, response.getOutputStream(), verifyCode);
+		} catch (IOException e) {
+			log.error(e);
+		} 
 	}
 
 }
