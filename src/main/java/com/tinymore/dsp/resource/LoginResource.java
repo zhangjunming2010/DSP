696556ff957e5846d22bf429f988cae32a749978
zhangjunming2010 <18696785349@163.com>
2017-03-29 17:03:08 +0800

    验证码生成时候讲值放入到cookie中

--- src/main/java/com/tinymore/dsp/resource/LoginResource.java
+++ src/main/java/com/tinymore/dsp/resource/LoginResource.java
@@ -2,16 +2,17 @@ package com.tinymore.dsp.resource;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tinymore.dsp.utils.VerifyCodeUtil;
 
@@ -22,20 +23,26 @@ public class LoginResource {
 	
 	private static final Logger log = (Logger) LogManager.getLogger(LoginResource.class);
 	
+	/**
+	 * 验证码获取
+	 * @param request
+	 * @param response
+	 */
 	@RequestMapping(value = "/getCode",method = RequestMethod.GET,produces="application/json; charset=utf-8")
-	public void getCode(HttpServletRequest request, HttpServletResponse response) {
-		response.setHeader("Pragma", "No-cache"); 
-        response.setHeader("Cache-Control", "no-cache"); 
-        response.setDateHeader("Expires", 0); 
-        response.setContentType("image/jpeg"); 
-           
+	public void getCode(HttpServletResponse response) {
+		// 设置响应的类型格式为图片格式  
+	    response.setContentType("image/jpeg");  
+	    //禁止图像缓存。  
+	    response.setHeader("Pragma", "no-cache");  
+	    response.setHeader("Cache-Control", "no-cache");  
+	    response.setDateHeader("Expires", 0);
+        
         //生成随机字串 
         String verifyCode = VerifyCodeUtil.generateVerifyCode(4); 
-        //存入会话session 
-        HttpSession session = request.getSession(true); 
-        //删除以前的
-        session.removeAttribute("verCode");
-        session.setAttribute("verCode", verifyCode.toLowerCase()); 
+        Cookie cookie = new Cookie("verifyCode", verifyCode.toLowerCase());
+        cookie.setMaxAge(3 * 60);// 设置为30min
+        cookie.setPath("/");
+        response.addCookie(cookie);
         //生成图片 
         int w = 100, h = 42; 
         try {
@@ -44,5 +51,16 @@ public class LoginResource {
 			log.error(e);
 		} 
 	}
-
+	
+	/**
+	 * 用户登录
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping(value = "/userLogin",method = RequestMethod.POST,produces="application/json; charset=utf-8")
+	@ResponseBody
+	public void userLogin(@RequestBody String params) {
+		System.out.println(params);
+	}
+	
 }
